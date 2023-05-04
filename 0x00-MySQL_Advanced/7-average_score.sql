51da4e2486342771c68fb38bdc93ff6292ef97f4
Romha Shwangzaw <romhas@gmail.com>
2023-05-04 15:48:05 +0300

    Bug Fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,11 @@
 -- This script creates a stored procedure `ComputeAverageScoreForUser`
 -- that computes and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 UPDATE users
 SET average_score = (
 	SELECT AVG(score)
-	FROM corrections
-	WHERE user_id = user_id
+	FROM corrections AS C
+	WHERE C.user_id = user_id
 )
 WHERE id = user_id;
